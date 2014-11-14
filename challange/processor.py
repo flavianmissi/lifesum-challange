503f5378c94416c73e057dcd6979cd811c141fe3
Flavia Missi <flaviamissi@gmail.com>
2014-11-13 22:13:42 -0200

    starts receiving key array for processing multiple keys on the data at the same time - not totally functional yet

--- challange/processor.py
+++ challange/processor.py
@@ -16,15 +16,15 @@ class SimpleMapReduce(object):
         self.worker = worker
         self.reduce_fn = reduce_fn
 
-    def map(self, data, key):
+    def map(self, data, keys):
         """
         Maps data to an {id: times} dictionary where times is the
-        occurrences of `key`.
-        Returns a list of those dicts.
+        occurrences of each key in `key` list.
+        Returns a list of the resulting dicts.
         """
         if not data:
             raise ValueError("You should first set SimpleMapReduce().data")
-        p = Process(target=self.worker, args=(data, key, self.namespace))
+        p = Process(target=self.worker, args=(data, keys, self.namespace))
         p.start()
         p.join()
         return Counter(self.namespace.mapping)
@@ -56,11 +56,21 @@ def worker(data, key, namespace):
     namespace.mapping = mapping
 
 
-def wrapper(obj_key):
-    return map_fn(*obj_key)
+def wrapper(obj_keys):
+    """
+    An "alternative" way to pass more than one parameter into a map function
+    """
+    return map_fn(*obj_keys)
 
 
-def map_fn(obj, key):
-    if key in obj.keys():
-        return obj[key]
+def map_fn(obj, keys):
+    """
+    Searches for `keys` in the dict `obj`.
+    Returns the value for each key in `keys`.
+
+    #TODO: add +1 for each obj[key] found in a {key: count} shared dict
+    """
+    for key in keys:
+        if key in obj.keys() and key in keys:
+            return obj[key]
     return
--- tests/test_processor.py
+++ tests/test_processor.py
@@ -7,10 +7,10 @@ class SimpleMapReduceTest(unittest.TestCase):
     def setUp(self):
         self.processor = SimpleMapReduce(worker, None)
 
-    def test_map_counts_how_many_entries_for_a_given_key(self):
+    def test_map_counts_how_many_entries_for_one_key(self):
         data = [{"food_id": 1}, {"food_id": 1}, {"food_id": 3}, {"food_id": 7}]
 
-        result = self.processor.map(data=data, key="food_id")
+        result = self.processor.map(data=data, keys=["food_id"])
         expected = {1: 2, 3: 1, 7: 1}
 
         self.assertDictEqual(result, expected)
@@ -19,7 +19,7 @@ class SimpleMapReduceTest(unittest.TestCase):
         data = [{"food_id": 1}, {"food_id": 1}, {"food_id": 2,"category_id": 3}, {"food_id": 7}]
         self.processor.data = data
 
-        result = self.processor.map(data=data, key="food_id")
+        result = self.processor.map(data=data, keys=["food_id"])
         expected = {1: 2, 2: 1, 7: 1}
 
         self.assertDictEqual(result, expected)
