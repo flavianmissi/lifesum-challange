b4d64c164b827ecb96c7bb7e767ea28cefcd770d
Flavia Missi <flaviamissi@gmail.com>
2014-11-16 18:41:38 -0200

    doing quiet an ugly thing in __main__, trying to control stuff out
    and possible finding a deadlock. but maybe it's just really slow... not
    sure.

--- challange/__main__.py
+++ challange/__main__.py
@@ -1,42 +1,55 @@
 from processor import SimpleMapReduce, map_worker, reduce_worker
+from semaphore import TimedSemaphore
 from requests_pool import ActiveRequestsPool, worker
 import multiprocessing
+import resource
 
+
+soft_limit, _ = resource.getrlimit(resource.RLIMIT_NPROC)
+SEMAPHORE = multiprocessing.Semaphore(soft_limit)
 
 MAX_REQS_PER_SECOND = 5
 
 
 def main():
-    max_limit = 5000
+    max_limit = 500000
     queue = multiprocessing.Queue()
-    semaphore = multiprocessing.Semaphore(MAX_REQS_PER_SECOND)
+    semaphore = TimedSemaphore(MAX_REQS_PER_SECOND)
     request_pool = ActiveRequestsPool()
     offset = 0
     limit = 300
     step = 300
     times = int(round(max_limit / float(step)))  # divides per step
     jobs = []
-    for t in range(times):
-        args = (semaphore, request_pool, queue, offset, limit)
-        p = multiprocessing.Process(target=worker, args=args)
-        jobs.append(p)
-        p.start()
-        offset = limit
-        limit += step
-
-    # wait for at least 5 jobs to complete
-    # this way we don't pass through the queue when it is not
-    # filled in yet.
-    for i in range(5):
-        jobs[0].join()
 
     most_food = 100
     most_cat = 5
     mapreduce = SimpleMapReduce(map_worker, reduce_worker)
     keys_bymost = [("food_id", most_food), ("category_id", most_cat)]
     reduced = None
-    while not queue.empty():
-        reduced = mapreduce.process(keys_bymost, queue)
+
+    with SEMAPHORE:
+        for t in range(times):
+            args = (semaphore, request_pool, queue, offset, limit)
+            p = multiprocessing.Process(target=worker, args=args)
+            jobs.append(p)
+            p.start()
+            offset = limit
+            limit += step
+
+            if not queue.empty():
+                reduced = mapreduce.process(keys_bymost, queue)
+
+        #import pdb;pdb.set_trace()
+        while not queue.empty():
+            reduced = mapreduce.process(keys_bymost, queue)
+
+        for job in jobs:
+            if job.is_alive():
+                job.join(5)
+                if not queue.empty():
+                    reduced = mapreduce.process(keys_bymost, queue)
+                job.terminate()
 
     reduced_foodid_values = reduced["food_id"].values()
     reduced_catid_values = reduced["category_id"].values()
