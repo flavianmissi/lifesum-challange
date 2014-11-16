09cc25e1ca25186a2442b7c0c60184a23e531b3f
Flavia Missi <flaviamissi@gmail.com>
2014-11-16 14:40:02 -0200

    now calls to the module runs the map reduce
    still need to improve the output though

--- challange/__main__.py
+++ challange/__main__.py
@@ -1,23 +1,65 @@
-from dataset import data_chunk
-from processor import SimpleMapReduce, map_worker
+from processor import SimpleMapReduce, map_worker, reduce_worker
+from requests_pool import ActiveRequestsPool, worker
 import multiprocessing
 
 
+MAX_REQS_PER_SECOND = 5
+
 
 def main():
-    p = SimpleMapReduce(map_worker, None, multiprocessing.Queue())
-    results = {"food_id": {}}
+    max_limit = 5000
+    queue = multiprocessing.Queue()
+    semaphore = multiprocessing.Semaphore(MAX_REQS_PER_SECOND)
+    request_pool = ActiveRequestsPool()
+    offset = 0
+    limit = 300
     step = 300
-    for limit in range(step, 900, step):  # for now
-        data = data_chunk()
-        result = p.map(data, keys=["food_id"])
+    times = int(round(max_limit / float(step)))  # divides per step
+    jobs = []
+    for t in range(times):
+        args = (semaphore, request_pool, queue, offset, limit)
+        p = multiprocessing.Process(target=worker, args=args)
+        jobs.append(p)
+        p.start()
+        offset = limit
+        limit += step
 
-        for k, v in result.items():
-            if k in results["food_id"].keys():
-                results["food_id"][k] += v
-            else:
-                results["food_id"][k] = v
+    # wait for at least 5 jobs to complete
+    # this way we don't pass through the queue when it is not
+    # filled in yet.
+    for i in range(5):
+        jobs[0].join()
 
-    print(results)
+    most_food = 100
+    most_cat = 5
+    mapreduce = SimpleMapReduce(map_worker, reduce_worker)
+    keys_bymost = [("food_id", most_food), ("category_id", most_cat)]
+    reduced = None
+    while not queue.empty():
+        reduced = mapreduce.process(keys_bymost, queue)
+
+    reduced_foodid_values = reduced["food_id"].values()
+    reduced_catid_values = reduced["category_id"].values()
+
+    reduced_foodid_values.sort()
+    reduced_catid_values.sort()
+
+    print("MOST {} FOOD_IDs:".format(most_food))
+    print("FOOD_ID     OCCURRENCES")
+    for v2 in reduced_foodid_values:
+        for k, v in reduced["food_id"].items():
+            if v == v2:
+                print("{}       :  {}".format(k, v))
+                break
+
+    print("======================================================================================")
+    print("MOST {} CATEGORY_IDs:".format(most_cat))
+    print("CATEGORY_ID     OCCURRENCES")
+    for v2 in reduced_catid_values:
+        for k, v in reduced["category_id"].items():
+            if v == v2:
+                print("{}           :  {}".format(k, v))
+                break
+
 
 main()
